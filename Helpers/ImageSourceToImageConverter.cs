1c883fa2ef29049f52d7361aeba0484cb1ca74df
titidatiti <jtw0925@gmail.com>
2025-11-29 20:41:24 +0800

    Optimize the visual effect of the pop-up view box after left-clicking

---
+++ Helpers/ImageSourceToImageConverter.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Globalization;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace TaskbarFolderShortcut.Helpers
+{
+    public class ImageSourceToImageConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is ImageSource source)
+            {
+                return new System.Windows.Controls.Image
+                {
+                    Source = source,
+                    Width = 16,
+                    Height = 16
+                };
+            }
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
